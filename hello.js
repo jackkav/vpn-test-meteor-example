327afc64a41ea9e5ed1e18853f200902f2b56313
jackkav <jackkav@gmail.com>
2015-05-14 10:26:52 +0800

    tidying up

--- hello.js
+++ hello.js
@@ -1,27 +1,22 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+  Session.setDefault('position', 0);
 
   Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+    place: function () {
+      return Session.get('position');
     }
   });
 
   Template.hello.events({
     'click button': function () {
-      // increment the counter when button is clicked
-      //Session.set('counter', Session.get('counter') + 1);
-      //  var r = Meteor.call('callMe');
-      //Session.set('counter', 3);
         Meteor.call('callMe', function(err, result){
             if (err) {
                 alert(err);
             } else {
                 var r = EJSON.parse(result);
                 var location = r.city+", " +r.country_name;
-                Session.set('counter', location);
-                $('.changeme').append(location+'<br/>');
+                Session.set('position', location);
+                $('.list').append(location+'<br/>');
             }
         });
 
